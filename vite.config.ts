1d382c821b67e55e98f8226ca44e4211625f7bbd
dengbinbox <dengbinbox@gmail.com>
2026-05-14 12:27:52 +0800

    update: 更新路由,更新logo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/costrict-static/resource-calculate/',
+  base: '/resource/',
   server: {
     host: '0.0.0.0',  // 监听所有网卡，支持远程访问
     port: 5173,
